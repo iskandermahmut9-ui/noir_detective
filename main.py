752e26cc33aed0c7323a054aa345da206ff69d71
iskandermahmut9-ui <iskandermahmut9@gmail.com>
2026-01-09 18:03:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ TG_TOKEN_NOIR = os.getenv("TG_TOKEN_NOIR")
 TG_TOKEN_SOUL = os.getenv("TG_TOKEN_SOUL")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-MODEL_NAME = "llama3-70b-8192"
+MODEL_NAME = "llama-3.3-70b-versatile"
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
